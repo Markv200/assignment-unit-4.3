9883d61888504994552e061ba6fc4107aaf9ccbe
Mark Velesaca <mark.velesaca@gmail.com>
2024-08-25 02:44:26 -0500

    Function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,33 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
 
+function addItem(item) {
+    basket.push(item);  
+    return true;        
+}
+console.log(`Basket should be empty: ${basket}`);
+console.log(addItem('Pineapples'));
+console.log(addItem('Apples'));
+console.log(addItem('Berries'));
+console.log(addItem('Bananas'));
+console.log(`Basket is now with ${basket}`);
 
+function listItems(){
+    for(let i=0; i<basket.length; i++){
+    console.log(basket[i]);
+    }
+}
+listItems();
 
+function empty(){
+    basket.length = 0;
+}
 
+console.log('Basket before', basket);
+empty();
+console.log('Basket after will be an empty array:', basket);
 
 
 
